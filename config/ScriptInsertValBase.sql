4a68b325fad7b568d189bb399e99c4ce3759518b
sacha markides <sacha.markides@orange.fr>
2017-09-04 14:15:03 +0200

    stash

--- config/ScriptInsertValBase.sql
+++ config/ScriptInsertValBase.sql
@@ -1,33 +1,33 @@
-INSERT INTO Candidate VALUES ('DRAGNIR','Natsu',NOW(),'1 rue de la liberté','natsu.dragnir@orange.fr',null,'Bonjour je suis natsu DRAGNIR ','Nat','pwd','candidate');
-INSERT INTO Candidate VALUES ('NEYMAR','Jean',NOW(),'1 rue de PSG', 'jean.neymar@orange.fr',null,'Bonjour je vaux 222 millions €', 'Jean', 'foot','candidate' );
+INSERT INTO Candidate VALUES (null,'DRAGNIR','Natsu',NOW(),'1 rue de la liberté','natsu.dragnir@orange.fr',null,'Bonjour je suis natsu DRAGNIR ','Nat','pwd','candidate');
+INSERT INTO Candidate VALUES (null,'NEYMAR','Jean',NOW(),'1 rue de PSG', 'jean.neymar@orange.fr',null,'Bonjour je vaux 222 millions €', 'Jean', 'foot','candidate' );
 
-INSERT INTO Enterprise VALUES ('QUATAR',0102030405, '1 rue du quatar','PARIS','lesrichesduMoyenOrient.com','lesrichesduMoyenOrient@pretentieux.com','Bonjour je possede une carte bleue non je voulais dire une carte dieu',null,'lesrichesduMoyenOrient@twitter.com','lesrichesduMoyenOrient@linkedin.com','Recherche de footballeur sans le bac','enterprise','dieuxDuFoot','petrole');
-INSERT INTO Enterprise VALUES ('FAIRYTAIL',0607080910, '1 rue de Magnolia','MAGNOLIA','fairytail.com','fairytail@pauvre.com','Bonjour je suis une guilde',null,'fairytail@twitter.com','fairytail@linkedin.com','Recherche de mage funs','enterprise','makarof','fee');
-INSERT INTO Enterprise VALUES ('SCIENCEINFUSE',9874561236, '1 rue du savoir','RENNES','lascienceinfuse.com','lascienceinfuse@pretentieux.com','Je sais tout',null,'lascienceinfuse@twitter.com','lascienceinfuse@linkedin.com','Recherche de savoir être ','enterprise','Eisnstein','prof');
-INSERT INTO Enterprise VALUES ('ONEPIECE',8569741252, '1 rue de Redline','FUSCHIA','lespiratesdunouveaumonde.com','lespiratesdunouveaumonde@pirate.com','Bonjour je suis le roi des pirates',null,'lespiratesdunouveaumonde@twitter.com','lespiratesdunouveaumonde@linkedin.com','Recherche de pirate','enterprise','chapeauDePaille','luffy');
-INSERT INTO Enterprise VALUES ('LOL',4578129632, '1 rue de riot games','LOS ANGELES','gamers.com','gamers@nolife.com','Bonjour je recherche des developpers/gamers',null,'gamers@twitter.com','gamers@linkedin.com','Recherche de joueurs très mauvais','enterprise','allstar','nexus');
-INSERT INTO Enterprise VALUES ('TROLL',4525658579, '1 rue du cest pas ici','TROLLCITY','.com','cestpasici@vienspas.com','Bonjour je ne recherche pas des trolls cest faux',null,'cestpasici@twitter.com','cestpasici@linkedin.com','Recheche personne ','enterprise','carleric','non');
+INSERT INTO Enterprise VALUES (null,'QUATAR',0102030405, '1 rue du quatar','PARIS','lesrichesduMoyenOrient.com','lesrichesduMoyenOrient@pretentieux.com','Bonjour je possede une carte bleue non je voulais dire une carte dieu',null,'lesrichesduMoyenOrient@twitter.com','lesrichesduMoyenOrient@linkedin.com','Recherche de footballeur sans le bac','enterprise','dieuxDuFoot','petrole');
+INSERT INTO Enterprise VALUES (null,'FAIRYTAIL',0607080910, '1 rue de Magnolia','MAGNOLIA','fairytail.com','fairytail@pauvre.com','Bonjour je suis une guilde',null,'fairytail@twitter.com','fairytail@linkedin.com','Recherche de mage funs','enterprise','makarof','fee');
+INSERT INTO Enterprise VALUES (null,'SCIENCEINFUSE',9874561236, '1 rue du savoir','RENNES','lascienceinfuse.com','lascienceinfuse@pretentieux.com','Je sais tout',null,'lascienceinfuse@twitter.com','lascienceinfuse@linkedin.com','Recherche de savoir être ','enterprise','Eisnstein','prof');
+INSERT INTO Enterprise VALUES (null,'ONEPIECE',8569741252, '1 rue de Redline','FUSCHIA','lespiratesdunouveaumonde.com','lespiratesdunouveaumonde@pirate.com','Bonjour je suis le roi des pirates',null,'lespiratesdunouveaumonde@twitter.com','lespiratesdunouveaumonde@linkedin.com','Recherche de pirate','enterprise','chapeauDePaille','luffy');
+INSERT INTO Enterprise VALUES (null,'LOL',4578129632, '1 rue de riot games','LOS ANGELES','gamers.com','gamers@nolife.com','Bonjour je recherche des developpers/gamers',null,'gamers@twitter.com','gamers@linkedin.com','Recherche de joueurs très mauvais','enterprise','allstar','nexus');
+INSERT INTO Enterprise VALUES (null,'TROLL',4525658579, '1 rue du cest pas ici','TROLLCITY','.com','cestpasici@vienspas.com','Bonjour je ne recherche pas des trolls cest faux',null,'cestpasici@twitter.com','cestpasici@linkedin.com','Recheche personne ','enterprise','carleric','non');
 
-INSERT INTO Skill VALUES ('JAVA','Langage');
-INSERT INTO Skill VALUES ('SQL','Langage');
-INSERT INTO Skill VALUES ('C','Langage');
-INSERT INTO Skill VALUES ('C++','Langage');
-INSERT INTO Skill VALUES ('Python','Langage');
-INSERT INTO Skill VALUES ('HTML','Langage');
-INSERT INTO Skill VALUES ('CSS','Langage');
-INSERT INTO Skill VALUES ('Javascript','Langage');
-INSERT INTO Skill VALUES ('Eclipse','Logiciel');
-INSERT INTO Skill VALUES ('StarUML','Logiciel');
-INSERT INTO Skill VALUES ('Git Bash','Logiciel');
+INSERT INTO Skill VALUES (null,'JAVA','Langage');
+INSERT INTO Skill VALUES (null,'SQL','Langage');
+INSERT INTO Skill VALUES (null,'C','Langage');
+INSERT INTO Skill VALUES (null,'C++','Langage');
+INSERT INTO Skill VALUES (null,'Python','Langage');
+INSERT INTO Skill VALUES (null,'HTML','Langage');
+INSERT INTO Skill VALUES (null,'CSS','Langage');
+INSERT INTO Skill VALUES (null,'Javascript','Langage');
+INSERT INTO Skill VALUES (null,'Eclipse','Logiciel');
+INSERT INTO Skill VALUES (null,'StarUML','Logiciel');
+INSERT INTO Skill VALUES (null,'Git Bash','Logiciel');
 
-INSERT INTO Job VALUES ('Développeur','Recherche un développeur java',' ',1245,null,null);
-INSERT INTO Job VALUES ('Mage','Recherche un mage de feu',' ',9874,null,null);
-INSERT INTO Job VALUES ('Footballeur','Recherche un footballeur qui ne possede pas le bac et qui de preference ne sais pas écrire',' ',4321,null,null);
-INSERT INTO Job VALUES ('Prof','Recherche un prof qui a la science infuse',' ',4587,null,null);
-INSERT INTO Job VALUES ('Pirate','Recherche un pirate sympa et riche',' ',7452,null,null);
-INSERT INTO Job VALUES ('Développeur/gamer','Recherche un développeur qui joue à league of legend et qui rage jamais',' ',1245,null,null);
-INSERT INTO Job VALUES ('Troll','On vous prendra pas pas,la peine de postuler',' ',8562,null,null);
+INSERT INTO Job VALUES (null,'Développeur','Recherche un développeur java',' ',1245,null,null);
+INSERT INTO Job VALUES (null,'Mage','Recherche un mage de feu',' ',9874,null,null);
+INSERT INTO Job VALUES (null,'Footballeur','Recherche un footballeur qui ne possede pas le bac et qui de preference ne sais pas écrire',' ',4321,null,null);
+INSERT INTO Job VALUES (null,'Prof','Recherche un prof qui a la science infuse',' ',4587,null,null);
+INSERT INTO Job VALUES (null,'Pirate','Recherche un pirate sympa et riche',' ',7452,null,null);
+INSERT INTO Job VALUES (null,'Développeur/gamer','Recherche un développeur qui joue à league of legend et qui rage jamais',' ',1245,null,null);
+INSERT INTO Job VALUES (null,'Troll','On vous prendra pas pas,la peine de postuler',' ',8562,null,null);
 
-INSERT INTO Administrator VALUES ('leboss','dujava','lebossdujava@orange.fr',7845126398,'admin','admin','admin');
+INSERT INTO Administrator VALUES (null,'leboss','dujava','lebossdujava@orange.fr',7845126398,'admin','admin','admin');
 
-INSERT INTO Headhunter Values ('Markides','Sacha',1245789656,'sacha.markides@orange.fr',null,'sacha.markides@twitter.fr','sacha.markides@linkedin.fr','bonjour je suis un headhunter', 'root','root','headhunter');
+INSERT INTO Headhunter Values (null,'Markides','Sacha',1245789656,'sacha.markides@orange.fr',null,'sacha.markides@twitter.fr','sacha.markides@linkedin.fr','bonjour je suis un headhunter', 'root','root','headhunter');
